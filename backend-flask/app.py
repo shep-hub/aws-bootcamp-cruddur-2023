53217d4e6907c4b84d62fc3b988a4f885b1fc9c6
shep-hub <shepchinhamo@gmail.com>
2023-04-29 05:09:32 +0000

    rename service and add missing middleware line

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -35,8 +35,8 @@ provider.add_span_processor(processor)
 
 # XRAY ---------
 xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
-
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 # Show this in the logs within the backend-flask app (STDOUT)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
